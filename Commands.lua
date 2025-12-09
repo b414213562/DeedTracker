11a56aa52260e239cac93562a3a2316960748ba5
Bill Van Ark <4hchm4xub2@snkmail.com>
2025-12-09 20:10:07 +0100

    Clarified wording on chat command help text.

--- Commands.lua
+++ Commands.lua
@@ -16,12 +16,18 @@ function RegisterCommands()
         end
     end
 
+    -- what to show when the user types /help deedtracker, /help deed, or /help deeds:
+    -- Note: LOTRO will not prepend with anything (other than date/time if that is enabled.) Example:
+    --   [12/09 08:08:47 PM] /deedtracker, /deed, or /deeds: Shows/hides the main Deed Tracker window.
     function cmdMainWin:GetHelp()
-        return "deedtracker: Shows/hides the main window.";
+        return "/deedtracker, /deed, or /deeds: Shows/hides the main Deed Tracker window.";
     end
 
+    -- what to show when the user types /help.
+    -- Note: LOTRO will prepend with all registered commands, so this will show as:
+    --   deedtracker,deed,deeds -- Shows/hides the main Deed Tracker window.
     function cmdMainWin:GetShortHelp()
-        return "deedtracker: Shows/hides the main window.";
+        return "Shows/hides the main Deed Tracker window.";
     end
 
     Turbine.Shell.AddCommand( "deedtracker;deed;deeds", cmdMainWin);
